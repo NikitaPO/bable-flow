40119d0a45a1deed0555fcd71f41dafd628f2772
NikitaPO <gigzim345@gmail.com>
2019-01-29 03:03:43 +0300

    +::

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function xor(x: boolean, y: boolean): boolean {
   return (x && !y) || (!x && y);
 }
 console.log(xor(1, 0));
+
+function square() {
+  return this * this;
+}
+console.log(5::square());
